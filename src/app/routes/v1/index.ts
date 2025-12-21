8fa732c1e1e9bce15b7d760ae739d746b652c91a
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2025-12-21 12:44:00 +0700

    remove module product and category

--- src/app/routes/v1/index.ts
+++ src/app/routes/v1/index.ts
@@ -2,14 +2,14 @@ import { Router } from "express";
 
 import userRoutes from "@/modules/user/user.route";
 import authRoutes from "@/modules/auth/auth.route";
-import categoryRoutes from "@/modules/category/category.route";
-import productRoutes from "@/modules/product/product.route";
+// import categoryRoutes from "@/modules/category/category.route";
+// import productRoutes from "@/modules/product/product.route";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
-router.use("/categories", categoryRoutes);
-router.use("/products", productRoutes);
+// router.use("/categories", categoryRoutes);
+// router.use("/products", productRoutes);
 
 export default router;
