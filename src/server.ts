b4e285746c68288117f4c2f20174aa44b9ef87c9
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2026-03-07 11:45:03 +0700

    fix port

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import "dotenv/config";
-
 import app from "./app/app";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
