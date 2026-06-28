acbad1e8344901c221182803d9bc93543d94e9fe
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2026-06-28 08:51:32 +0700

    clean: remove trash file

--- scripts/cleanup-refresh-token.ts
+++
@@ -1,8 +0,0 @@
-import { cleanupRefreshTokens } from "@/app/modules/auth/auth.service";
-// npm run cleanup:refresh-token
-
-(async () => {
-  const deleted = await cleanupRefreshTokens();
-  console.log(`[CLEANUP] Deleted ${deleted} refresh tokens`);
-  process.exit(0);
-})();
